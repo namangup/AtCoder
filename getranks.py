52cc6ab8df9dc13a43e3514c660e421825e2c852
namangup <namangup@iitk.ac.in>
2020-04-29 22:02:17 +0530

    Minor Changes

--- getranks.py
+++ getranks.py
@@ -3,19 +3,25 @@ import csv
 from scrape import gen_json
 from sys import argv
 
-contest = argv[1]
+usernames = []
+with open(argv[1],'r') as f:
+    fr = csv.reader(f, delimiter=',')
+    for row in fr:
+        usernames.append(row[1].strip())
+
+print(usernames)
+contest = argv[2]
 gen_json(contest)
 
 with open('ranklist.json', 'r') as f:
     data = json.load(f)
 
-username = [] # Get usernames from a file
 ranks = []
 f = open('ranks.csv', 'w')
 fw = csv.writer(f, quoting=csv.QUOTE_ALL)
 data = data["StandingsData"]
 for i in data:
-    if (i['UserName'] in username):
+    if (i['UserName'] in usernames):
         fw.writerow([i['UserName'], i['Rank']])
 
 f.close()
